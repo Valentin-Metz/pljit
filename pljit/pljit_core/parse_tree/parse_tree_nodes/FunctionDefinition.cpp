11eff5bdab7e9fde917228dbd3d35f553e559fbb
Valentin Metz <valentin.metz@tum.de>
2022-08-16 08:21:50 +0200

    Parse Tree

--- pljit/pljit_core/parse_tree/parse_tree_nodes/FunctionDefinition.cpp
+++ pljit/pljit_core/parse_tree/parse_tree_nodes/FunctionDefinition.cpp
@@ -1,7 +1,15 @@
 #include "FunctionDefinition.hpp"
+#include "../../lexer/tokens/LexerKeywordToken.hpp"
 
 namespace parse_tree {
-FunctionDefinition::FunctionDefinition(lexer::Lexer& l) {
+using namespace lexer;
+FunctionDefinition::FunctionDefinition(Lexer& l) {
+    LexerToken t{l.nextToken()};
 
+    switch (t.token_type) {
+        case lexer::LexerToken::Keyword: {
+            LexerKeywordToken k = static_cast<LexerKeywordToken>(t);
+        }
+    }
 }
 } // namespace parse_tree
---
+++ pljit/pljit_core/parse_tree/parse_tree_nodes/Keyword.hpp
@@ -0,0 +1,11 @@
+#ifndef PLJIT_KEYWORD_HPP
+#define PLJIT_KEYWORD_HPP
+
+namespace parse_tree {
+
+class Keyword {
+};
+
+} // namespace parse_tree
+
+#endif //PLJIT_KEYWORD_HPP
