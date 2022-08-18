e44fdde947c921a8375c28c3b394b71c81b7f703
Valentin Metz <valentin.metz@tum.de>
2022-08-18 16:24:05 +0200

    AST

--- pljit/pljit_core/ast/ast_nodes/Function.cpp
+++ pljit/pljit_core/ast/ast_nodes/Function.cpp
@@ -1,14 +1,15 @@
 #include "Function.hpp"
 #include "../../parse_tree/ParseTree.hpp"
 #include "../../parse_tree/parse_tree_nodes/ParseTreeStatement.hpp"
+#include "AssignmentStatement.hpp"
 #include "ReturnStatement.hpp"
 
 namespace ast {
 Function::Function(const parse_tree::StatementList& statement_list, source_code::SourceCode& source_code) {
     for (auto& statement : statement_list.statementList) {
-        if (statement.first.get()->assignmentExpression){
-
-        } else{
+        if (statement.first.get()->assignmentExpression) {
+            statements.push_back(std::make_unique<AssignmentStatement>(statement.first.get()->assignmentExpression.value().get()));
+        } else {
             statements.push_back(std::make_unique<ReturnStatement>(statement.first.get()->additiveExpression.value().second.get()));
         }
     }
