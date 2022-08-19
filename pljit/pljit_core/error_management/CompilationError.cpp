f8fec383fcf4b802c25f0bbaadf955fc73798fa9
Valentin Metz <valentin.metz@tum.de>
2022-08-19 06:32:47 +0200

    Error message print

--- pljit/pljit_core/error_management/CompilationError.cpp
+++ pljit/pljit_core/error_management/CompilationError.cpp
@@ -2,38 +2,54 @@
 #include <iostream>
 void CompilationError::print(source_code::SourceCode& source_code) {
     std::size_t line_number = 0;
-    std::size_t character_position = 0;
+    std::size_t last_linebreak = 0;
 
     for (std::size_t i = 0; i < source_code_reference.byte_index; ++i) {
-        if (source_code.source_code[i] == '\n') line_number++;
-    }
-    for (std::size_t i = 0; i < source_code_reference.byte_index; ++i) {
-        if (source_code.source_code[source_code_reference.byte_index - i] != '\n')
-            character_position++;
-        else
-            break;
+        if (source_code.source_code[i] == '\n') {
+            last_linebreak = i;
+            line_number++;
+        }
     }
 
-    std::cout << line_number << ":" << character_position << ": ";
+    std::size_t character_position = source_code_reference.byte_index - last_linebreak - 1;
+
+    // Human line numbers start at 1
+    std::cout << (line_number + 1) << ":" << character_position << ": ";
 
     switch (error_source) {
         case Lexer: {
-            std::cout << "Lexer error: " << error_message;
+            std::cout << "Lexer error:";
             break;
         }
         case ParseTree: {
-            std::cout << "ParseTree error: " << error_message;
+            std::cout << "ParseTree error:";
             break;
         }
         case SymbolTable: {
-            std::cout << "SymbolTable error: " << error_message;
+            std::cout << "SymbolTable error:";
             break;
         }
         case AST: {
-            std::cout << "AST error: " << error_message;
+            std::cout << "AST error:";
             break;
         }
     }
+    std::cout << "\n";
 
-    std::cout << std::endl;
+    // Print line where error occurred
+    for (std::size_t i = last_linebreak + 1; i < source_code.source_code.length() && source_code.source_code[i] != '\n'; ++i) {
+        std::cout << source_code.source_code[i];
+    }
+    std::cout << "\n";
+
+    // Underline erroneous position
+    for (std::size_t i = 0; i < character_position; ++i) {
+        std::cout << " ";
+    }
+    for (std::size_t i = 0; i < source_code_reference.reference_length; ++i) {
+        std::cout << "^";
+    }
+    std::cout << "\n";
+
+    std::cout << error_message << std::endl;
 }
