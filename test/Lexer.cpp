929a76257f92c49f08afabb73681b979c39b89b5
Valentin Metz <valentin.metz@tum.de>
2022-08-19 07:08:31 +0200

    PrintVisitor

--- test/Lexer.cpp
+++ test/Lexer.cpp
@@ -16,54 +16,54 @@ TEST(LexerTest, LexerValidSource) {
     Lexer l{c};
 
     // PARAM width, height, depth;\n
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Keyword);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Identifier);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Separator);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Identifier);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Separator);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Identifier);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Separator);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Keyword);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Identifier);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Separator);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Identifier);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Separator);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Identifier);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Separator);
 
     // VAR volume;\n
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Keyword);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Identifier);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Separator);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Keyword);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Identifier);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Separator);
 
     // CONST density = 2400;
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Keyword);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Identifier);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Declarator);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Literal);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Separator);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Keyword);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Identifier);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Declarator);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Literal);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Separator);
 
     // BEGIN\n
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Keyword);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Keyword);
 
     // volume := width * height * depth;\n
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Identifier);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Assignment);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Identifier);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Arithmetic);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Identifier);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Arithmetic);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Identifier);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Separator);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Identifier);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Assignment);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Identifier);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Arithmetic);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Identifier);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Arithmetic);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Identifier);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Separator);
 
     // RETURN density * volume\n
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Keyword);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Identifier);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Arithmetic);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Identifier);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Keyword);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Identifier);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Arithmetic);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Identifier);
 
     // END.
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Keyword);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Terminator);
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Error);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Keyword);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Terminator);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Error);
 }
 
 TEST(LexerTest, LexerEmptySource) {
     SourceCode c{SourceCode("")};
     Lexer l{c};
 
-    EXPECT_EQ(l.nextToken().token_type, LexerToken::Error);
+    EXPECT_EQ(l.nextToken()->token_type, LexerToken::Error);
 }
