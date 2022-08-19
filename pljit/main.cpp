c686821b9d1f85920de5d8a0495e6a271fdc018b
Valentin Metz <valentin.metz@tum.de>
2022-08-19 05:15:08 +0200

    AST

--- pljit/main.cpp
+++ pljit/main.cpp
@@ -1,3 +1,4 @@
+#include "pljit_core/ast/Ast.hpp"
 #include "pljit_core/lexer/Lexer.hpp"
 #include "pljit_core/parse_tree/ParseTree.hpp"
 #include "pljit_core/parse_tree/parse_tree_nodes/StatementList.hpp"
@@ -13,17 +14,18 @@ int main(int argc, char* argv[]) {
     std::ifstream ifs(argv[1]);
     std::string valid_source((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
 
-    source_code::SourceCode c{valid_source};
+    source_code::SourceCode source_code{valid_source};
 
     //CompilationError error = CompilationError(source_code::SourceCodeReference{0,5}, CompilationError::ParseTree, "Bad thing");
-    //error.print(c);
-    lexer::Lexer l{c};
+    //error.print(source_code);
+    lexer::Lexer l{source_code};
 
     try {
         parse_tree::ParseTree parse_tree{l};
         parse_tree.print();
+        ast::AST ast{parse_tree, source_code};
     } catch (CompilationError e) {
-        e.print(c);
+        e.print(source_code);
         std::cout << "error" << std::endl;
     }
 
