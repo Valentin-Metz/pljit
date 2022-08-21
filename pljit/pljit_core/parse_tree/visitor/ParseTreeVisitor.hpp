943f8e80899d2f8c90f19c84fed5fb8c14e0dbd7
Valentin Metz <valentin.metz@tum.de>
2022-08-21 23:11:24 +0200

    tests

---
+++ pljit/pljit_core/parse_tree/visitor/ParseTreeVisitor.hpp
@@ -0,0 +1,50 @@
+#ifndef PLJIT_PARSETREEVISITOR_HPP
+#define PLJIT_PARSETREEVISITOR_HPP
+
+namespace parse_tree {
+
+class AdditiveExpression;
+class AssignmentExpression;
+class CompoundStatement;
+class ConstantDeclaration;
+class DeclaratorList;
+class FunctionDefinition;
+class Identifier;
+class InitDeclarator;
+class InitDeclaratorList;
+class Literal;
+class MultiplicativeExpression;
+class ParameterDeclaration;
+class PrimaryExpression;
+class Statement;
+class StatementList;
+class TerminalSymbol;
+class UnaryExpression;
+class VariableDeclaration;
+
+class ParseTreeVisitor {
+    public:
+    virtual ~ParseTreeVisitor() = default;
+    virtual void visit(const AdditiveExpression& node) const = 0;
+    virtual void visit(const AssignmentExpression& node) const = 0;
+    virtual void visit(const CompoundStatement& node) const = 0;
+    virtual void visit(const ConstantDeclaration& node) const = 0;
+    virtual void visit(const DeclaratorList& node) const = 0;
+    virtual void visit(const FunctionDefinition& node) const = 0;
+    virtual void visit(const Identifier& node) const = 0;
+    virtual void visit(const InitDeclarator& node) const = 0;
+    virtual void visit(const InitDeclaratorList& node) const = 0;
+    virtual void visit(const Literal& node) const = 0;
+    virtual void visit(const MultiplicativeExpression& node) const = 0;
+    virtual void visit(const ParameterDeclaration& node) const = 0;
+    virtual void visit(const PrimaryExpression& node) const = 0;
+    virtual void visit(const Statement& node) const = 0;
+    virtual void visit(const StatementList& node) const = 0;
+    virtual void visit(const TerminalSymbol& node) const = 0;
+    virtual void visit(const UnaryExpression& node) const = 0;
+    virtual void visit(const VariableDeclaration& node) const = 0;
+};
+
+} // namespace parse_tree
+
+#endif //PLJIT_PARSETREEVISITOR_HPP
