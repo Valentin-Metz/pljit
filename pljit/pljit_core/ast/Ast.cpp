df70e0c0f329ff67d7f3aeb792f77f4e8f7c8d65
Valentin Metz <valentin.metz@tum.de>
2022-08-21 12:19:53 +0200

    cleanup

--- pljit/pljit_core/ast/Ast.cpp
+++ pljit/pljit_core/ast/Ast.cpp
@@ -1,13 +1,13 @@
 #include "Ast.hpp"
-#include "ast/optimization/ConstantPropagationOptimizationPass.hpp"
-#include "ast/optimization/DeadCodeEliminationOptimizationPass.hpp"
 #include "ast_core/AstVisitor.hpp"
 #include "ast_core/ExecutionTable.hpp"
+#include "optimization/ConstantPropagationOptimizationPass.hpp"
+#include "optimization/DeadCodeEliminationOptimizationPass.hpp"
 #include "print/AstPrintVisitor.hpp"
 
 namespace ast {
 
-AST::AST(parse_tree::ParseTree& parse_tree, source_code::SourceCode& source_code) : symbolTable(parse_tree, source_code), function(std::make_unique<Function>(parse_tree.root.compound_statement.value().statementList, symbolTable, source_code)), sourceCode(std::move(source_code)) {}
+AST::AST(parse_tree::ParseTree& parse_tree, source_code::SourceCode& source_code) : symbolTable(parse_tree, source_code), function(std::make_unique<Function>(parse_tree.root.compound_statement.value().statementList, symbolTable, source_code)) {}
 AST::~AST() = default;
 
 void AST::accept(AstVisitor& visitor) {
--- pljit/pljit_core/ast/Ast.hpp
+++ pljit/pljit_core/ast/Ast.hpp
@@ -1,37 +1,41 @@
 #ifndef PLJIT_AST_HPP
 #define PLJIT_AST_HPP
 
-#include "../parse_tree/ParseTree.hpp"
 #include "ast_core/ExecutionTable.hpp"
 #include "ast_core/SymbolTable.hpp"
 #include "ast_nodes/Function.hpp"
+#include "parse_tree/ParseTree.hpp"
 namespace pljit {
 class PLjit_FunctionHandle;
 }
 
 namespace ast {
 
+/// Abstract syntax tree
 class AST {
     friend class pljit::PLjit_FunctionHandle;
+
+    // The execution table is used to evaluate an ast with user-given parameters
+    std::optional<const ExecutionTable> executionTable;
+    void generateExecutionTable();
+    ExecutionTable getExecutionTable();
 
     public:
     SymbolTable symbolTable;
     std::unique_ptr<Function> function;
-    const source_code::SourceCode& sourceCode;
 
+    // Constructors
     AST(parse_tree::ParseTree& parse_tree, source_code::SourceCode& source_code);
     ~AST();
 
+    // Visitor
     void accept(AstVisitor& visitor);
+
+    // Print this ast in DOT format
     void print();
+
+    // Optimize this ast
     void optimize();
-
-    private:
-    // The execution table manages variables during execution
-    std::optional<const ExecutionTable> executionTable;
-
-    void generateExecutionTable();
-    ExecutionTable getExecutionTable();
 };
 
 } // namespace ast
